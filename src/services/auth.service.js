10c8dcfd67523a65208c286f8f42373bdbf64341
medlaq777 <m.laqsoumi@gmail.com>
2025-12-16 13:26:24 +0100

    LOG-43 LOG-48 LOG-49 LOG-50 Complet all Trailer Crud Parts and all functionalities

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,19 +1,16 @@
 import apiClient from '../config/axios';
 
 const authService = {
-    // Login user
     login: async (email, password) => {
         const response = await apiClient.post('/login', { email, password });
         return response.data;
     },
 
-    // Register new user
     register: async (userData) => {
         const response = await apiClient.post('/register', userData);
         return response.data;
     },
 
-    // Get current user profile
     getProfile: async () => {
         const response = await apiClient.get('/profile');
         return response.data;
--- src/services/truck.service.js
+++ src/services/truck.service.js
@@ -1,31 +1,25 @@
 import apiClient from '../config/axios';
 
 const truckService = {
-    // Get all trucks
     getAll: async () => {
         const response = await apiClient.get('/trucks');
         return response.data;
     },
 
-    // Get single truck by ID
     getById: async (id) => {
         const response = await apiClient.get(`/trucks/${id}`);
         return response.data;
     },
-
-    // Create new truck
     create: async (truckData) => {
         const response = await apiClient.post('/trucks', truckData);
         return response.data;
     },
 
-    // Update truck
     update: async (id, truckData) => {
         const response = await apiClient.put(`/trucks/${id}`, truckData);
         return response.data;
     },
 
-    // Delete truck
     delete: async (id) => {
         const response = await apiClient.delete(`/trucks/${id}`);
         return response.data;
